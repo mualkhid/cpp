66ebbe087256ef23e48c65f9c0da05a2a0f75fb1
mualkhid <162097353+mualkhid@users.noreply.github.com>
2025-03-09 20:38:25 +0000

    cpp06

--- C06/ex02/Base.cpp
+++ C06/ex02/Base.cpp
@@ -1,68 +1,109 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Base.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mnassi <mnassi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/23 18:27:01 by mnassi            #+#    #+#             */
-/*   Updated: 2023/09/24 15:08:19 by mnassi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "Base.hpp"
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
 
-Base  *Base::generate(void) {
-	srand(time(0));
-	if ((rand()) % 3 == 0)
-		return (new A);
-	else if ((rand()) % 3 == 1)
-		return (new B);
-	return (new C);
+// Base  *Base::generate(void) {
+// 	srand(time(0));
+// 	if ((rand()) % 3 == 0)
+// 		return (new A);
+// 	else if ((rand()) % 3 == 1)
+// 		return (new B);
+// 	return (new C);
+// }
+
+// void	Base::identify(Base *p) {
+// 	Base *child = dynamic_cast<A*>(p);
+// 	if (child != NULL)
+// 		std::cout << "pointer is type A" << std::endl;
+// 	child = dynamic_cast<B*>(p);
+// 	if (child != NULL)
+// 		std::cout << "pointer is type B" << std::endl;
+// 	child = dynamic_cast<C*>(p);
+// 	if (child != NULL)
+// 		std::cout << "pointer is type C" << std::endl;
+// }
+
+// void	Base::identify(Base &p) {
+// 	try {
+// 		Base &child = dynamic_cast<A&>(p);
+// 		(void)child;
+// 		std::cout << "reference is type A" << std::endl;
+// 	}
+// 	catch (std::exception &e) {
+
+// 	}
+// 	try {
+// 		Base &child = dynamic_cast<B&>(p);
+// 		(void)child;
+// 		std::cout << "reference is type B" << std::endl;
+// 	}
+// 	catch (std::exception &e) {
+
+// 	}
+// 	try {
+// 		Base &child = dynamic_cast<C&>(p);
+// 		(void)child;
+// 		std::cout << "reference is type C" << std::endl;
+// 	}
+// 	catch (std::exception &e) {
+
+// 	}
+// }
+
+// Base::~Base(void) {
+// 	std::cout << "Destructor Base Called" << std::endl;
+// }
+
+
+
+
+Base *Base::generate(void) {
+    srand(time(0));
+    if ((rand()) % 3 == 0)
+        return (new A);
+    else if ((rand()) % 3 == 1)
+        return (new C);
+    return (new B);
 }
 
-void	Base::identify(Base *p) {
-	Base *tyca = dynamic_cast<A*>(p);
-	if (tyca != NULL)
-		std::cout << "pointer is type A" << std::endl;
-	tyca = dynamic_cast<B*>(p);
-	if (tyca != NULL)
-		std::cout << "pointer is type B" << std::endl;
-	tyca = dynamic_cast<C*>(p);
-	if (tyca != NULL)
-		std::cout << "pointer is type C" << std::endl;
+void Base::identify(Base *p) {
+    Base *child = dynamic_cast<A*>(p);
+    if (child != NULL)
+        std::cout << RED << "Pointer is type A" << RESET << std::endl;
+
+    child = dynamic_cast<B*>(p);
+    if (child != NULL)
+        std::cout << GREEN << "Pointer is type B" << RESET << std::endl;
+
+    child = dynamic_cast<C*>(p);
+    if (child != NULL)
+        std::cout << BLUE << "Pointer is type C" << RESET << std::endl;
 }
 
-void	Base::identify(Base &p) {
-	try {
-		Base &tyca = dynamic_cast<A&>(p);
-		(void)tyca;
-		std::cout << "reference is type A" << std::endl;
-	}
-	catch (std::exception &e) {
+void Base::identify(Base &p) {
+    try {
+        Base &child = dynamic_cast<A&>(p);
+        (void)child;
+        std::cout << RED << "Reference is type A" << RESET << std::endl;
+    }
+    catch (std::exception &e) {}
 
-	}
-	try {
-		Base &tyca = dynamic_cast<B&>(p);
-		(void)tyca;
-		std::cout << "reference is type B" << std::endl;
-	}
-	catch (std::exception &e) {
+    try {
+        Base &child = dynamic_cast<B&>(p);
+        (void)child;
+        std::cout << GREEN << "Reference is type B" << RESET << std::endl;
+    }
+    catch (std::exception &e) {}
 
-	}
-	try {
-		Base &tyca = dynamic_cast<C&>(p);
-		(void)tyca;
-		std::cout << "reference is type C" << std::endl;
-	}
-	catch (std::exception &e) {
-
-	}
+    try {
+        Base &child = dynamic_cast<C&>(p);
+        (void)child;
+        std::cout << BLUE << "Reference is type C" << RESET << std::endl;
+    }
+    catch (std::exception &e) {}
 }
 
 Base::~Base(void) {
-	std::cout << "Destructor Base Called" << std::endl;
+    std::cout << "Destructor Base Called" << std::endl;
 }
--- C06/ex02/Base.hpp
+++ C06/ex02/Base.hpp
@@ -1,19 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   Base.hpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mnassi <mnassi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/23 18:27:52 by mnassi            #+#    #+#             */
-/*   Updated: 2023/09/24 15:20:34 by mnassi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
 #ifndef BASE_HPP
 #define BASE_HPP
 
+#define RESET   "\033[0m"
+#define RED     "\033[31m"  // Red for A
+#define GREEN   "\033[32m"  // Green for B
+#define BLUE    "\033[34m"  // Blue for C
+
 #include <iostream>
+#include <cstdlib>
 
 class Base {
 	public :
