83ea8d04a3750af7b163dcd890d8ac0caf2b2005
Munia Alkhidir <mualkhid@lab1r1s13.42abudhabi.ae>
2025-03-12 12:46:16 +0400

    update

---
+++ CPP06/ex00/ScalarConverter.hpp
@@ -0,0 +1,29 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+#include <cmath>
+#include <limits>
+#include <iomanip>
+
+
+class ScalarConverter
+{
+    private:
+        ScalarConverter();
+        ScalarConverter(ScalarConverter const & src);
+        ScalarConverter& operator=(ScalarConverter const & src);
+        ~ScalarConverter();
+    public:
+        static void    toChar(std::string input);
+        static void    toInt(std::string input);
+        static void    toFloat(std::string input);
+        static void    toDouble(std::string input);
+
+        static void    convert( std::string input);
+};
+
+#endif
--- CPP06/ex00/main.cpp
+++ CPP06/ex00/main.cpp
@@ -1,23 +1,10 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mnassi <mnassi@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/10 14:08:57 by mnassi            #+#    #+#             */
-/*   Updated: 2023/09/17 17:36:22 by mnassi           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
 
-#include "Bureaucrat.hpp"
+#include "ScalarConverter.hpp"
 
-int main() {
-	try {
-		Bureaucrat	person("mohammed", 10);
-		std::cout << person << std::endl;
+int main(int ac, char **av) {
+	if (ac > 2 || !av[1]) {
+		std::cout << "Error! One argument only" << std::endl;
+		return (0);
 	}
-	catch (std::exception &e) {
-		std::cout << e.what() << std::endl;
-	}
+	ScalarConverter::convert(av[1]);
 }
